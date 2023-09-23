e3db5b1ea132982c77acc1790124636662b18075
Senibratov S <nlo_tomsk@mail.ru>
2023-09-23 20:17:30 +0700

    004-02

--- 004-stream/004-02/index.js
+++ 004-stream/004-02/index.js
@@ -2,30 +2,14 @@ const fs = require('fs')
 
 const fileName = process.argv.slice(2)
 
-console.log(fileName[0])
-
-//const readStream = fs.createReadStream(fileName[0])
-
 const readStream = fs.readFileSync(fileName[0], 'utf8').toString().trim().split("\n")
 
-console.log(readStream)
-
+let count = 0
 for (i in readStream) {
     const dataParsed = JSON.parse(readStream[i])
-    console.log(dataParsed);
+
+    if (dataParsed.result == dataParsed.input) {
+        count += 1
+    }
 }
-//const dataParsed = JSON.parse(readStream)
-
-//console.log(dataParsed)
-// let data
-// dataParsed
-//     .setEncoding('UTF-8')
-//     .on('data', (chank) => {
-//         data += chank
-//     })
-//     .on('end', () => {
-//         console.log('end', data)
-//     })
-
-
-console.log(`\nВсего игр: \n Выиграно/Проиграно: \n Процент выигрышей:\n`)
+console.log(`\nВсего игр: ${readStream.length} \n Выиграно/Проиграно: ${count}/${readStream.length - count}\n Процент выигрышей: ${(count * 100) / readStream.length} %\n`)
